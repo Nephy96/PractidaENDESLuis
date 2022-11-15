2433ffa2e64ee0652d2e467cdd5b24ea36ff61aa
alumno <alumno@DESKTOP-5FAD0MS>
2022-11-15 20:47:50 +0100

    Prueba Commit
    Prueba Commit

---
+++ PracticaGITLuis/src/Mojino.java
@@ -0,0 +1,10 @@
+
+public class Mojino {
+
+	public static void main(String[] args) {
+
+		System.out.println("Que dise el enlike no vea abe jajaja xDDDDD ostia k wapo e o no wno hasta luego");
+
+	}
+
+}
