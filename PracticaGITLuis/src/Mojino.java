3cb390e1d127eef58c4aa8d9fcb8c2109b8f2f68
alumno <alumno@DESKTOP-5FAD0MS>
2022-11-21 19:41:20 +0100

    Cambio mensaje 1
    He cambiado el mensaje 1 :D

--- PracticaGITLuis/src/Mojino.java
+++ PracticaGITLuis/src/Mojino.java
@@ -3,7 +3,7 @@ public class Mojino {
 
 	public static void main(String[] args) {
 
-		System.out.println("Que dise el enlike no vea abe jajaja xDDDDD ostia k wapo e o no wno hasta luego");
+		System.out.println("Conjunto de palabras coherente y bien redactada");
 		System.out.println("oTRA TONMTERIA");
 	}
 
