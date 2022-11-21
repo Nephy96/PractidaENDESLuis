967c47daf4106bfbd8b825c589fc5302e0fb83b7
profeRSG <rsangal205@g.educaand.es>
2022-11-21 19:34:43 +0100

    Mensaje añadido en el main
    Mensaje añadido en el main "Otra tonteria"

--- PracticaGITLuis/src/Mojino.java
+++ PracticaGITLuis/src/Mojino.java
@@ -4,7 +4,7 @@ public class Mojino {
 	public static void main(String[] args) {
 
 		System.out.println("Que dise el enlike no vea abe jajaja xDDDDD ostia k wapo e o no wno hasta luego");
-
+		System.out.println("oTRA TONMTERIA");
 	}
 
 }
